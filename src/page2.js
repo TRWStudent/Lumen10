628976fdbbd4a566d754e77fdeea20c485c0caf3
TRWStudent <kevin.meier@student.bvs.ch>
2026-01-30 14:44:42 +0100

    Updated page2.js

--- src/page2.js
+++ src/page2.js
@@ -3,7 +3,7 @@ function loadInsightsVideo() {
   const videoElement = videoSource.parentElement;
 
   if (videoSource && videoElement) {
-    videoSource.src = '/videos/insights-video.mp4';
+    videoSource.src = 'https://ppgwkuxotjeadkwzulju.supabase.co/storage/v1/object/public/videos/2026-01-30T13-17-39_cinematic_aerial_watermarked.mp4';
     videoElement.load();
   }
 }
